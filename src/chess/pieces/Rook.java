c9a16a2868899ccfba3db666642eb663b76890ac
domssilva <domenique_silva@hotmail.com>
2021-06-05 16:22:23 +0200

    Placing pieces on the board

---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,19 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece {
+
+	public Rook(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		// yes a string, not a char. Because this will be printed on the UI grid.
+		return "R";
+	}
+	
+}
